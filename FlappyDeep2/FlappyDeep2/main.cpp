5be9cea0190e0b8eac8a8c329431baa29268996e
Doge815 <42283470+Doge815@users.noreply.github.com>
2020-02-20 10:32:23 +0100

    create project

---
+++ FlappyDeep2/FlappyDeep2/main.cpp
@@ -0,0 +1,25 @@
+#include <SFML/Graphics.hpp>
+
+const int size = 500;
+const int dimensions = 2;
+
+int main()
+{
+    sf::RenderWindow *window = new sf::RenderWindow(sf::VideoMode(size, size), "FlappyDeep2", sf::Style::Titlebar | sf::Style::Close);
+	
+    while (window->isOpen())
+    {
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window->close();
+        }
+
+        window->clear(sf::Color::Black);
+        window->display();
+    }
+
+    return 0;
+}
+//g++ -c main.cpp && g++ main.o -o FlappyDeep2 -lsfml-graphics -lsfml-window -lsfml-system && ./FlappyDeep2
