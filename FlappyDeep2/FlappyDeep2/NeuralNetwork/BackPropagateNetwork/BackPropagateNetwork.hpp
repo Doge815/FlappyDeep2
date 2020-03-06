799af8d1a458aa384b43284ca20f5bb556d88852
Doge815 <42283470+Doge815@users.noreply.github.com>
2020-03-06 12:36:51 +0100

    BackPropagateNetworkCollection Constructor

--- FlappyDeep2/FlappyDeep2/NeuralNetwork/BackPropagateNetwork/BackPropagateNetwork.hpp
+++ FlappyDeep2/FlappyDeep2/NeuralNetwork/BackPropagateNetwork/BackPropagateNetwork.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
 #include "../INetwork.hpp"
+#include "../NetworkShape.hpp"
 
 enum LayerType {Hidden, Input, Output};
 
 class BackPropagateNetwork : INetwork
 {
     private:
-      BackPropagateLayer* Layers;  
+      BackPropagateLayer* Layers;
+
     public:
+        BackPropagateNetwork(NetworkShape shape);
         void Inputs(double* values);
         double* Outputs();
 
--- FlappyDeep2/FlappyDeep2/NeuralNetwork/BackPropagateNetwork/BackPropagateNetworkCollection.hpp
+++ FlappyDeep2/FlappyDeep2/NeuralNetwork/BackPropagateNetwork/BackPropagateNetworkCollection.hpp
@@ -5,13 +5,34 @@
 #include "BackPropagateNetwork.hpp"
 
 #include<vector>
+#include<cstdlib>
 
 using namespace std;
 
 class BackPropagateNetworkCollection : NeuralNetworkCollection
 {
     BackPropagateNetwork* nets;
+    int amountOfNetworks;
     public:
+        BackPropagateNetworkCollection(int amount, NetworkShape shape);
         vector<INetwork*> GetNetworks();
         void Evolve();
 };
+
+BackPropagateNetworkCollection::BackPropagateNetworkCollection(int amount, NetworkShape shape)
+{
+    nets = (BackPropagateNetwork*)malloc(sizeof(amount));
+    for(int i = 0; i < amount; i++)
+    {
+        nets[i] = BackPropagateNetwork(shape);
+    }
+}
+
+void BackPropagateNetworkCollection::Evolve()
+{
+    for (size_t i = 0; i < amountOfNetworks; i++)
+    {
+        nets[i].Learn();
+    }
+    
+}
